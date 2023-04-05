a0adb0f1ff9e8ab0ba13de777b6cf7c537f8ec15
anika-jane <112495457+anika-jane@users.noreply.github.com>
2023-04-05 11:32:34 -0400

    Moved over information from CodeSandbox to VScode

---
+++ src/SearchEngine.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function SearchEngine() {
+  const [city, setCity] = useState(" ");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState(" ");
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5f472b7acba333cd8a035ea85a0d4d4c&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
+      <button type="Submit">Search</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}</li>
+          <li>
+            <img src={weather.icon} alt="weather icon" />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
